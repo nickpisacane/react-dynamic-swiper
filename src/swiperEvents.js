a969330fdfa996822500773c05a1f680558720eb
Nick Pisacane <pisacanen@gmail.com>
2016-08-05 15:14:05 -0700

    swiper events

---
+++ src/swiperEvents.js
@@ -0,0 +1,39 @@
+import { PropTypes } from 'react'
+
+export const events = [
+  'onInit',
+  'onSlideChangeStart',
+  'onSlideChangeEnd',
+  'onSlideNextStart',
+  'onSlideNextEnd',
+  'onSlidePrevStart',
+  'onSlidePrevEnd',
+  'onTransitionStart',
+  'onTransitionEnd',
+  'onTouchStart',
+  'onTouchMove',
+  'onTouchMoveOpposite',
+  'onSliderMove',
+  'onTouchEnd',
+  'onClick',
+  'onTap',
+  'onDoubleTap',
+  'onImagesReady',
+  'onProgress',
+  'onReachBeginning',
+  'onReachEnd',
+  'onDestroy',
+  'onSetTranslate',
+  'onSetTransition',
+  'onAutoplay',
+  'onAutoplayStart',
+  'onAutoplayStop',
+  'onLazyImageLoad',
+  'onLazyImageReady',
+  'onPaginationRendered'
+]
+
+export const EventPropTypes = events.reduce((obj, event) => {
+  obj[event] = PropTypes.func
+  return obj
+}, {})
