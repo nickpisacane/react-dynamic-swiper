ad2e65c32c3a50e3de2aefa0d95ae964060409bf
Nick Pisacane <pisacanen@gmail.com>
2018-03-25 20:04:44 -0700

    support wrapping content in a fragment when being portaled (for duplicates)

--- src/Slide.js
+++ src/Slide.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import omit from 'lodash.omit'
 import cx from 'classnames'
 
-export default function Slide ({ children, className, ...rest }) {
+// NOTE: The Slide component should be thought of as a light wrapper for content.
+// Consider the use of fragments below, adding things like an `click` handler, or
+// any other DOM event handlers through React properties will simply not work. 
+// Should this trigger a warning?
+export default function Slide ({
+  children,
+  className,
+  onActive,
+  isPortaled = false,
+  ...rest
+}) {
+  if (isPortaled) {
+    return <React.Fragment>{children}</React.Fragment>
+  }
+
   return (
-    <div className={cx('swiper-slide', className)} {...omit(rest, 'onActive')}>
+    <div className={cx('swiper-slide', className)} {...rest}>
       {children}
     </div>
   )
@@ -14,5 +27,6 @@ export default function Slide ({ children, className, ...rest }) {
 Slide.propTypes = {
   onActive: PropTypes.func,
   children: PropTypes.node,
-  className: PropTypes.string
+  className: PropTypes.string,
+  isPortaled: PropTypes.bool
 }
